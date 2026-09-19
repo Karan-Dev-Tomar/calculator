f93c929f6401e231945f532f00984497f680e6e6
Karan-Dev-Tomar <karandevtomar@gmai.com>
2026-09-19 20:02:44 +0530

    code added

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # calculator
-This is a calculator made with using java. It has if inside for which is inside a while loop, so it contains nested loops  
+This is a calculator made with using java. It has if statements inside for loops which is inside a while loop, so is based on nested loops, this could also have been done by while and switch or do while loop. 
+Made by Karan a first year student at sharda university.
---
+++ calculator.java
@@ -0,0 +1,93 @@
+package calculator;
+import java.util.Scanner;
+public class calculator{
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO KARAN'S CALCULATOR");
+        while(true){
+            float a;
+            char b;
+            float c;
+            String e;
+            float d;
+            System.out.print("enter your number: ");
+            Scanner sc= new Scanner(System.in);
+            a=sc.nextInt();
+            System.out.print("operator: ");
+            Scanner op= new Scanner(System.in);
+            b=op.next().charAt(0);
+            System.out.print("next number: ");
+            Scanner nm= new Scanner(System.in);
+            c=nm.nextInt();
+            if (b == '+'){
+                d=a+c;
+                System.out.println((a + c));
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e=="n"){
+                        break;
+                    }
+            }
+            else if (b=='-'){
+                d=a-c;
+                System.out.println(d);
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e=="n"){
+                        break;
+                    }
+            }
+            else if (b=='*'|| b=='x'){
+                d=a*c;
+                System.out.println(d);
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e=="n"){
+                        break;
+                    }
+            }
+            else if (b=='/'){
+                d=a/c;
+                System.out.println(d);
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e=="n"){
+                        break;
+                    }
+            }
+            else if(b=='p'){
+                d=a/c;
+                d=d*100;
+                System.out.println(d+"%");
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e!="y"||e!="Y"){
+                        break;
+                    }
+            }
+            else if (b=='%'){
+                d=a%c;
+                System.out.println(d);
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e=="n"){
+                        break;
+                    }
+            }
+            else{
+                System.out.println("invalid input");
+                System.out.print("do you want to continue (y/n): ");
+                Scanner ou=new Scanner(System.in);
+                e=ou.nextLine();
+                    if (e=="n"){
+                        break;
+                    }
+            }
+        }
+    }
+}
